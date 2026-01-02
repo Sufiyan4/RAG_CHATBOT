19a6e3522c61b1d9e37db07a365274a32bfeb127
Sufiyan4 <85099755+Sufiyan4@users.noreply.github.com>
2026-01-03 01:23:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
